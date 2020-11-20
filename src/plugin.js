6dd19a87608f7c5a1073f7401a3389cfa23a76d1
Cody Olsen <cody.olsen@finn.no>
2020-11-20 16:06:43 +0100

    chore: kinda ready

--- src/plugin.js
+++ src/plugin.js
@@ -1,3 +1,4 @@
+const valueParser = require('postcss-value-parser');
 // match custom property inclusions
 // @TODO optimize to skip vars with fallbacks already
 const customPropertiesRegExp = /(^|[^\w-])var\([\W\w]+\)/;
@@ -5,16 +6,34 @@ const customPropertiesRegExp = /(^|[^\w-])var\([\W\w]+\)/;
 // whether the declaration should be potentially transformed
 const isTransformableDecl = (decl) => customPropertiesRegExp.test(decl.value);
 
+// eslint-disable-next-line no-empty-pattern
 module.exports = ({} = {}) => ({
   postcssPlugin: 'postcss-custom-properties-fallback',
   prepare() {
     // @TODO check importFrom arguments
 
+    const customProperties = { '--color': 'black' };
+
     return {
       Declaration(node) {
         if (isTransformableDecl(node)) {
-          const originalValue = decl.node;
-          console.log({ originalValue });
+          const parsed = valueParser(node.value);
+
+          parsed.walk((node) => {
+            // Only deal with vars without a fallback
+            if (node.type !== 'function' || node.nodes.length !== 1) {
+              return;
+            }
+            const fallback = customProperties[node.nodes[0].value];
+            if (fallback) {
+              node.nodes.push(
+                { type: 'divider', value: ',', after: ' ' },
+                { type: 'word', value: fallback }
+              );
+            }
+          });
+
+          node.value = parsed.toString();
         }
       },
     };
---
+++ tap-snapshots/test-plugin.js-TAP.test.js
@@ -0,0 +1,14 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/plugin.js TAP plugin() - simple module - should replace normalize.css with CDN URL > simple example 1`] = `
+
+.test {
+	color: var(--color,black);
+}
+
+`
