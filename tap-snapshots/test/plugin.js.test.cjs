fa688ed0dee3ac54439ef4657787d9ce81eb2c2d
Cody Olsen <stipsan@gmail.com>
2022-03-16 21:27:22 +0100

    chore: add another test case

--- tap-snapshots/test/plugin.js.test.cjs
+++ tap-snapshots/test/plugin.js.test.cjs
@@ -7,26 +7,34 @@
 'use strict'
 exports[`test/plugin.js TAP plugin() - list module - should replace the layered box-shadow correctly > list example 1`] = `
 
-.test {
-  box-shadow: var(--shadow-depth-1,0 1px 3px rgba(0,0,0,0.12) , 0 1px 2px rgba(0,0,0,0.24));
-}
-
+      .test {
+        box-shadow: var(--shadow-depth-1,0 1px 3px rgba(0,0,0,0.12) , 0 1px 2px rgba(0,0,0,0.24));
+      }
+    
 `
 
 exports[`test/plugin.js TAP plugin() - resurcive module - should replace normalize.css with CDN URL > recursive example 1`] = `
 
-.test {
-  color: var(--primary, var(--color,black));
-}
+      .test {
+        color: var(--primary, var(--color,black));
+      }
+    
+`
 
+exports[`test/plugin.js TAP plugin() - rgba > functional notation example 1`] = `
+
+    .test {
+      color: rbga(var(--color-rgb,45 , 45 , 45), 0.5);
+    }
+  
 `
 
 exports[`test/plugin.js TAP plugin() - simple module > simple example 1`] = `
 
-.test {
-  color: var(--color,black);
-  background-color: var(--primary, blue);
-  outline-color: var(--primary,yellow);
-}
-
+    .test {
+      color: var(--color,black);
+      background-color: var(--primary, blue);
+      outline-color: var(--primary,yellow);
+    }
+  
 `
--- test/plugin.js
+++ test/plugin.js
@@ -1,26 +1,7 @@
-const tap = require('tap')
+const tap = require('tap');
 const postcss = require('postcss');
 const plugin = require('../src/plugin');
 
-const simple = `
-.test {
-  color: var(--color);
-  background-color: var(--primary, blue);
-  outline-color: var(--primary);
-}
-`;
-
-const recursive = `
-.test {
-  color: var(--primary, var(--color));
-}
-`;
-
-const list = `
-.test {
-  box-shadow: var(--shadow-depth-1);
-}
-`;
 /*
  * When running tests on Windows, the output code get some extra \r on each line.
  * Remove these so snapshots work on all OSes.
@@ -28,6 +9,14 @@ const list = `
 const clean = (str) => str.split('\r').join('');
 
 tap.test('plugin() - simple module ', async (t) => {
+  const simple = `
+    .test {
+      color: var(--color);
+      background-color: var(--primary, blue);
+      outline-color: var(--primary);
+    }
+  `;
+
   const { css } = await postcss(
     plugin({
       importFrom: {
@@ -43,6 +32,12 @@ tap.test('plugin() - simple module ', async (t) => {
 tap.test(
   'plugin() - resurcive module - should replace normalize.css with CDN URL',
   async (t) => {
+    const recursive = `
+      .test {
+        color: var(--primary, var(--color));
+      }
+    `;
+
     const { css } = await postcss(
       plugin({
         importFrom: {
@@ -59,10 +54,19 @@ tap.test(
 tap.test(
   'plugin() - list module - should replace the layered box-shadow correctly',
   async (t) => {
+    const list = `
+      .test {
+        box-shadow: var(--shadow-depth-1);
+      }
+    `;
+
     const { css } = await postcss(
       plugin({
         importFrom: {
-          customProperties: { '--shadow-depth-1': '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)' },
+          customProperties: {
+            '--shadow-depth-1':
+              '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+          },
         },
       })
     ).process(list, { from: undefined });
@@ -71,3 +75,21 @@ tap.test(
     t.end();
   }
 );
+
+tap.test('plugin() - rgba', async (t) => {
+  const rgba = `
+    .test {
+      color: rbga(var(--color-rgb), 0.5);
+    }
+  `;
+  const { css } = await postcss(
+    plugin({
+      importFrom: {
+        customProperties: { '--color-rgb': '45,45,45' },
+      },
+    })
+  ).process(rgba, { from: undefined });
+
+  t.matchSnapshot(clean(css), 'functional notation example');
+  t.end();
+});
