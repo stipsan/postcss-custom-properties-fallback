094cf500fdc49d2e81cb26beffb69e2277ebbc55
Cody Olsen <stipsan@gmail.com>
2022-03-16 20:30:20 +0100

    chore: prepping release

--- tap-snapshots/test/plugin.js.test.cjs
+++ tap-snapshots/test/plugin.js.test.cjs
@@ -5,6 +5,14 @@
  * Make sure to inspect the output below.  Do not ignore changes!
  */
 'use strict'
+exports[`test/plugin.js TAP plugin() - list module - should replace the layered box-shadow correctly > list example 1`] = `
+
+.test {
+  box-shadow: var(--shadow-depth-1,0 1px 3px rgba(0,0,0,0.12) , 0 1px 2px rgba(0,0,0,0.24));
+}
+
+`
+
 exports[`test/plugin.js TAP plugin() - resurcive module - should replace normalize.css with CDN URL > recursive example 1`] = `
 
 .test {
